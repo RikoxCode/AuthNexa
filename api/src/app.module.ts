692d8f6145927f2c7ed01841d1449a5121deb8d5
Levin Zimmermann <111433435+RikoxCode@users.noreply.github.com>
2024-01-26 08:08:36 +0100

    Update app.module.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { MailModule } from './feats/mail/mail.module';
-// import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { UsersModule } from './feats/users/users.module';
-// import { AuthModule } from './feats/auth/auth.module';
+import { MailModule } from './feats/mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './feats/users/users.module';
+import { AuthModule } from './feats/auth/auth.module';
 import { AvailableCheckModule } from './feats/available-check/available-check.module';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   envFilePath: '.env',
-    //   isGlobal: true,
-    // }),
-    // MongooseModule.forRoot(process.env.MONGO_URI, {
-    //   autoIndex: true,
-    // }),
-    // MailModule,
-    // UsersModule,
-    // AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      autoIndex: true,
+    }),
+    MailModule,
+    UsersModule,
+    AuthModule,
     AvailableCheckModule,
   ],
   controllers: [AppController],
